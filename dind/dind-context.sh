870893445ac6397b3e91c549eac671d035ad0760
garyplattenburg <gary.plattenburg@gmail.com>
2023-05-12 14:39:19 -0400

    fixed missing port

--- dind/dind-context.sh
+++ dind/dind-context.sh
@@ -1,3 +1,3 @@
 docker context rm -f dind
-docker context create dind --description "docker in k8s" --docker "host=tcp://$1,ca=temp\client\ca.pem,cert=temp\client\cert.pem,key=temp\client\key.pem,skip-tls-verify=1"
+docker context create dind --description "docker in k8s" --docker "host=tcp://$1:443,ca=temp\client\ca.pem,cert=temp\client\cert.pem,key=temp\client\key.pem,skip-tls-verify=1"
 docker context use dind
